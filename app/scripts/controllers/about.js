7bbd4920a4b78015823ef5c3ab007246c83200d1
ttran96 <ttran96@gatech.edu>
2017-10-14 06:57:34 -0400

    Project Setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name doctorFinderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the doctorFinderApp
+ */
+angular.module('doctorFinderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name doctorFinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the doctorFinderApp
+ */
+angular.module('doctorFinderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
